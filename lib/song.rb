b72b97211272c2d04538b3f6a7233a1d9e7c6ccd
Tony Ho <ho.tonym@gmail.com>
2018-05-17 03:48:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,4 +28,45 @@ class Song
       element.name == song_name
     end
   end
+
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      Song.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort { |x, y| x.name <=> y.name }
+  end
+
+  def Song.new_from_filename(filename)
+    song = Song.new
+    names = filename.split(/(?:.mp3)|(?: - )/)
+    song.artist_name = names[0]
+    song.name = names[1]
+    song
+  end
+
+  def Song.create_from_filename(filename)
+    song = Song.new
+    names = filename.split(/[.]|(?: - )/)
+    song.artist_name = names[0]
+    song.name = names[1]
+    @@all << song
+  end
+
+  def Song.destroy_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
 end
